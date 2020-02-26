5471b0413030a5925a374ff7d31fc2c10f3d85d7
User Name <artur.it2914@gmail.com>
2020-02-26 15:50:10 +0200

    Create: simple form template

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor (props) {
+    super( props );
+    this.state = {
+      data: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+      },
+      user: null,
+    };
+  }
+
+  handleChange = e => {
+    console.dir( e.target );
+    this.setState( {
+                     data: {
+                       ...this.state.data,
+                       [e.target.name]: e.target.value
+                     },
+                   } );
+  };
+
+  submitHandler = e => {
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify( this.state.data )
+    };
+
+    fetch( 'http://192.168.0.106:3000/authorization/sign_up', options )
+      .then( response => response.json() )
+      .then( user => {
+        this.setState( { user } );
+      } )
+      .catch( console.dir );
+    e.preventDefault();
+  };
+
+  render () {
+    return (
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        maxWidth: '320px'
+      }}>
+        <input type="text" value={this.state.data.firstName}
+               onChange={this.handleChange} placeholder="Name" name="firstName"/>
+        <input type="text" value={this.state.data.lastName}
+               onChange={this.handleChange} name="lastName" placeholder="Surname"/>
+        <input type="email" value={this.state.data.email}
+               onChange={this.handleChange} name="email" placeholder="email"/>
+        <input type="password" value={this.state.data.password}
+               onChange={this.handleChange} name="password" placeholder="password"/>
+        <input type="button" onClick={this.submitHandler} value={'Sign up'}/>
+      </div>
+    );
+  }
 }
 
 export default App;
